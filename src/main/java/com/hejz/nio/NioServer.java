0789348e12a02eee756cd218c9a038905c20c90c
hejz <75412985@qq.com>
2021-04-23 16:19:36 +0800

    bio

--- src/main/java/com/hejz/nio/NioServer.java
+++ src/main/java/com/hejz/nio/NioServer.java
@@ -1,7 +1,5 @@
 package com.hejz.nio;
 
-import ch.qos.logback.core.BasicStatusManager;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -12,13 +10,15 @@ import java.util.Iterator;
 import java.util.List;
 
 public class NioServer {
+    //存放SocketChannel的集合——这样有一个坏处，无论多少连接都需要遍历查询有值的进行处理，使用selector——多路复用器来代替，并提高性能
     private static List<SocketChannel> chanelList=new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         //创建NIO serverSocketChannel，与BIO的serversocket类似
         ServerSocketChannel serverSocketChannel=ServerSocketChannel.open();
+        //serverSocketChannel中的socket绑定端口地址
         serverSocketChannel.socket().bind(new InetSocketAddress(9000));
-        //设置为非阻塞
+        //设置为非阻塞,
         serverSocketChannel.configureBlocking(false);
         System.out.println("服务启动成功");
         while (true){
@@ -36,6 +36,7 @@ public class NioServer {
                 if(read>0){
                     System.out.println("接收到消息："+new String(byteBuffer.array()));
                 }else if(read==-1){
+                    //移除里面为空值的socketChanel
                     iterator.remove();
                     System.out.println("客户端断开连接。");
                 }
