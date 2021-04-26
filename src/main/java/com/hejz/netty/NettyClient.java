d611bcc516802eea964cd36bf4f5f668607faa61
hejz <75412985@qq.com>
2021-04-26 08:06:54 +0800

    netty clent编写

---
+++ src/main/java/com/hejz/netty/NettyClient.java
@@ -0,0 +1,39 @@
+package com.hejz.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * 客户端服务
+ */
+
+public class NettyClient {
+    public static void main(String[] args) {
+        EventLoopGroup group = new NioEventLoopGroup(1);
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new NettyClientHandler());
+                        }
+                    });
+            System.out.println("netty client start ..");
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 9000).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            group.shutdownGracefully();
+        }
+
+    }
+
+}
--- src/main/java/com/hejz/netty/NettyServerHandler.java
+++ src/main/java/com/hejz/netty/NettyServerHandler.java
@@ -6,7 +6,16 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
+/**
+ * 自定义Handler需要继承netty规定好的某个HandlerAdapter(规范)
+ */
 public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+    /**
+     * 读取客户端发过来的消息
+     * @param ctx 上下文对象含有channel,管道pipeline
+     * @param msg 接收到客户端发过来的信息
+     * @throws Exception
+     */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         System.out.println("服务器读取纯种 " + Thread.currentThread().getName());
@@ -14,9 +23,25 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
         System.out.println("客户端发送消息是：" + buffer.toString(CharsetUtil.UTF_8));
     }
 
+    /**
+     * 数据读取完毕处理方法
+     * @param ctx
+     * @throws Exception
+     */
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
         ByteBuf buf = Unpooled.copiedBuffer("HelloClient", CharsetUtil.UTF_8);
         ctx.writeAndFlush(buf);
+    }
+
+    /**
+     * 处理异常时要关闭上下文
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
     }
 }
